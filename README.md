05a8e876e48ec9ee854ebb125dfd3d05dfaac20b
Nils Jonsson <nils@alumni.rice.edu>
2016-01-14 21:54:15 -0600

    Fix typos in readme and Hex package metadata

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 Your project may contain files that serve as examples to the developer, or
 templates. Such files are intended to be copied by the developer for use as
-configuration and the like. The mix tasks provided here enable you to easily
+configuration and the like. The Mix tasks provided here enable you to easily
 find, copy, and check the freshness of example files and your copies of them.
--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule ExampleFiles.Mixfile do
 
     Your project may contain files that serve as examples to the developer, or
     templates. Such files are intended to be copied by the developer for use as
-    configuration and the like. The mix tasks provided here enable you to easily
+    configuration and the like. The Mix tasks provided here enable you to easily
     find, copy, and check the freshness of example files and your copies of them.
     """
   end
